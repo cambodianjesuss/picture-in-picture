f492a115bf8037d6e2ec1810aa9093c6b9227640
Boi Soth <cambodianjesuss@gmail.com>
2023-01-07 00:32:43 -0800

    created starting point for selecting media stream

---
+++ script.js
@@ -0,0 +1,14 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt user to select a media stream, pass to video element & play
+const selectMediaStream = async () => {
+  try {
+ff
+  } catch(error){
+    console.log('Error here: ', error)
+  }
+}
+
+// On Load
+selectMediaStream()
