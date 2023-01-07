b5f41931e2baf97ca3c4f5d4c9d3afb7f7c43892
Boi Soth <cambodianjesuss@gmail.com>
2023-01-07 00:52:00 -0800

    share screen on load then click on start to go into PiP mode

--- script.js
+++ script.js
@@ -14,5 +14,14 @@ const selectMediaStream = async () => {
   }
 }
 
+button.addEventListener('click', async ()=> {
+  // Disable button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
+
 // On Load
 selectMediaStream()
